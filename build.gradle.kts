78e91442f7dd5505abc33c38737ccc8029e4d654
eve0415 <40632436+eve0415@users.noreply.github.com>
2025-01-19 13:33:42 +0900

    chore: update to 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,58 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
-val ktlint: Configuration by configurations.creating
-
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "net.eve0415"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
-    maven {
-        name = "papermc-repo"
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.12.1")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.12.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    paperweight {
+        paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+    }
 
-    ktlint("com.pinterest:ktlint:0.49.1")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.20.0")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.20.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 }
 
 kotlin {
-    jvmToolchain(17)
-    sourceSets.all {
-        languageSettings {
-            languageVersion = "2.0"
-        }
-    }
+    jvmToolchain(21)
 }
 
 tasks {
     compileKotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-            apiVersion.set(KotlinVersion.KOTLIN_1_8)
-            languageVersion.set(KotlinVersion.KOTLIN_1_8)
+            jvmTarget.set(JvmTarget.JVM_21)
+            apiVersion.set(KotlinVersion.KOTLIN_2_1)
+            languageVersion.set(KotlinVersion.KOTLIN_2_1)
         }
     }
 
     compileTestKotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-            apiVersion.set(KotlinVersion.KOTLIN_1_8)
-            languageVersion.set(KotlinVersion.KOTLIN_1_8)
+            jvmTarget.set(JvmTarget.JVM_21)
+            apiVersion.set(KotlinVersion.KOTLIN_2_1)
+            languageVersion.set(KotlinVersion.KOTLIN_2_1)
         }
     }
 
     withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
     }
-    
+
     processResources {
         inputs.property("version", project.version)
         from(sourceSets.main.get().resources.srcDirs) {
@@ -66,34 +62,11 @@ tasks {
         }
     }
 
-    check {
-        dependsOn("ktlintCheck")
+    build {
+        dependsOn(reobfJar)
     }
 
-    register<JavaExec>("ktlintCheck") {
-        inputs.files(project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt")))
-        outputs.dir("${project.buildDir}/reports/ktlint")
-
-        group = "verification"
-        description = "Check Kotlin code style."
-        classpath = ktlint
-        mainClass.set("com.pinterest.ktlint.Main")
-        args = listOf(
-            "src/**/*.kt",
-            "--reporter=plain",
-            "--reporter=checkstyle,output=$buildDir/reports/ktlint/checkstyle-report.xml"
-        )
-    }
-
-    register<JavaExec>("ktlintFormat") {
-        inputs.files(project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt")))
-        outputs.dir("${project.buildDir}/reports/ktlint")
-
-        group = "formatting"
-        description = "Fix Kotlin code style deviations."
-        classpath = ktlint
-        mainClass.set("com.pinterest.ktlint.Main")
-        args = listOf("-F", "src/**/*.kt")
-        jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    runServer {
+        minecraftVersion("1.21")
     }
 }
